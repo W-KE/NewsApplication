f73734a4b68eab405af422a1d8304ea7ac9eb5ef
W-KE <wangke97@outlook.com>
2019-04-03 10:55:05 +1300

    添加项目文件。

---
+++ NewsApplication/Models/NewsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsApplication.Models
+{
+    public class NewsModel
+    {
+        public string NewsId { get; set; }
+
+        public string NewsTitle { get; set; }
+
+        public string NewsDescription { get; set; }
+
+        public string NewsDate { get; set; }
+
+        public string NewsLink { get; set; }
+
+        public string NewsSource { get; set; }
+
+        public string NewsAuthor { get; set; }
+    }
+}
---
+++ NewsApplication/Repository/NewsRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NewsApplication.Models;
+using System.Web;
+
+namespace NewsApplication.Repository
+{
+    public class NewsRepository
+    {
+        public static List<NewsModel> GetProductList()
+        {
+            HtmlWeb website = new HtmlWeb();
+            List<NewsModel> prod_list = new List<NewsModel>();
+
+            prod_list.Add(new NewsModel()
+            {
+                NewsId = "www.stuff.co.nz/111746865",
+                NewsTitle = "'We're in crazytown now': Spectacular fall of WND, the king of conspiracy sites",
+                NewsDescription = "US website founder built an empire stoking \"birther\" theories, but now it's imploding.",
+                NewsDate = "Tue, 02 Apr 2019 18:51:51 GMT",
+                NewsLink = "https://www.stuff.co.nz/business/world/111746865/were-in-crazytown-now-spectacular-fall-of-wnd-the-king-of-rightwing-conspiracy-sites.html",
+                NewsSource = "Stuff.co.nz",
+                NewsAuthor = "MANUEL ROIG-FRANZIA"
+            });
+
+            prod_list.Add(new NewsModel()
+            {
+                NewsId = "www.stuff.co.nz/111746865",
+                NewsTitle = "'We're in crazytown now': Spectacular fall of WND, the king of conspiracy sites",
+                NewsDescription = "US website founder built an empire stoking \"birther\" theories, but now it's imploding.",
+                NewsDate = "Tue, 02 Apr 2019 18:51:51 GMT",
+                NewsLink = "https://www.stuff.co.nz/business/world/111746865/were-in-crazytown-now-spectacular-fall-of-wnd-the-king-of-rightwing-conspiracy-sites.html",
+                NewsSource = "Stuff.co.nz",
+                NewsAuthor = "MANUEL ROIG-FRANZIA"
+            });
+
+            prod_list.Add(new NewsModel()
+            {
+                NewsId = "www.stuff.co.nz/111746865",
+                NewsTitle = "'We're in crazytown now': Spectacular fall of WND, the king of conspiracy sites",
+                NewsDescription = "US website founder built an empire stoking \"birther\" theories, but now it's imploding.",
+                NewsDate = "Tue, 02 Apr 2019 18:51:51 GMT",
+                NewsLink = "https://www.stuff.co.nz/business/world/111746865/were-in-crazytown-now-spectacular-fall-of-wnd-the-king-of-rightwing-conspiracy-sites.html",
+                NewsSource = "Stuff.co.nz",
+                NewsAuthor = "MANUEL ROIG-FRANZIA"
+            });
+
+            return prod_list;
+        }
+    }
+}
